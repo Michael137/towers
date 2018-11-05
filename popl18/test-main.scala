c09dadd981b6ebc47c4e695c27845f38f08dcbd8
Michael Buch <michaelbuch12@gmail.com>
2018-11-05 00:29:29 +0000

    begin stack machine

--- popl18/test-main.scala
+++ popl18/test-main.scala
@@ -1,12 +1,13 @@
 object TestMain {
   def main(args: Array[String]) {
-    Base.test()
-    Lisp.test()
-    Pink.test()
-    Pink_CPS.test()
-    Pink_clambda.test()
-    Matcher.test()
-    Bench.test()
+//    Base.test()
+//    Lisp.test()
+//    Pink.test()
+//    Pink_CPS.test()
+//    Pink_clambda.test()
+//    Matcher.test()
+    Stack_Machine.test()
+//    Bench.test()
     println("DONE")
   }
 }

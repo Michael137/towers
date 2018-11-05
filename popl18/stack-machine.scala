f4f3f52de20837239b1b64bf95959a9c2d3ddcbd
Michael Buch <michaelbuch12@gmail.com>
2018-11-05 01:36:37 +0000

    continue on stack

--- popl18/stack-machine.scala
+++ popl18/stack-machine.scala
@@ -3,23 +3,41 @@
 import Base._
 import Lisp._
 
+/*
+ * Components:
+ *
+ * * EIP (instruction pointer)
+ * * ALU
+ * * RAM
+ * * Stack
+ * * Call stack
+ * * Operations:
+ */
 object Stack_Machine {
+  /*
   val stack_machine_src = """
   (let star_loop (lambda star_loop m (lambda _ c (maybe-lift (lambda inner_loop s
   (if (eq?  (maybe-lift 'yes) (m s)) (maybe-lift 'yes)
   (if (eq?  (maybe-lift 'done) (car s)) (maybe-lift 'no)
   (if (eq?  '_ c) (inner_loop (cdr s))
   (if (eq?  (maybe-lift c) (car s)) (inner_loop (cdr s)) (maybe-lift 'no)))))))))
-(let match_here (lambda match_here r (lambda _ s (if (eq?  'done (car r)) (maybe-lift 'yes)
-  (let m (lambda _ s
-      (if (eq?  '_ (car r)) (if (eq?  (maybe-lift 'done) (car s)) (maybe-lift 'no) ((match_here (cdr r)) (cdr s)))
-      (if (eq?  (maybe-lift 'done) (car s)) (maybe-lift 'no)
-      (if (eq?  (maybe-lift (car r)) (car s)) ((match_here (cdr r)) (cdr s)) (maybe-lift 'no)))))
-    (if (eq?  'done (car (cdr r))) (m s)
-    (if (eq?  '* (car (cdr r))) (((star_loop (match_here (cdr (cdr r)))) (car r)) s) (m s)))))))
-(let match (lambda match r
-  (if (eq?  'done (car r)) (maybe-lift (lambda _ s (maybe-lift 'yes))) (maybe-lift (match_here r))))
-match)))"""
+    (let match_here (lambda match_here r (lambda _ s (if (eq?  'done (car r)) (maybe-lift 'yes)
+      (let m (lambda _ s
+          (if (eq?  '_ (car r)) (if (eq?  (maybe-lift 'done) (car s)) (maybe-lift 'no) ((match_here (cdr r)) (cdr s)))
+          (if (eq?  (maybe-lift 'done) (car s)) (maybe-lift 'no)
+          (if (eq?  (maybe-lift (car r)) (car s)) ((match_here (cdr r)) (cdr s)) (maybe-lift 'no)))))
+          (if (eq?  'done (car (cdr r))) (m s)
+          (if (eq?  '* (car (cdr r))) (((star_loop (match_here (cdr (cdr r)))) (car r)) s) (m s)))))))
+            (let match (lambda match r
+              (if (eq?  'done (car r)) (maybe-lift (lambda _ s (maybe-lift 'yes))) (maybe-lift (match_here r))))
+              match)
+    )
+  )
+  """
+  */
+  val stack_machine_src = """
+    
+  """
 
   val machine_src = s"(let maybe-lift (lambda _  e e) $stack_machine_src)"
   val machinec_src = s"(let maybe-lift (lambda _  e (lift e)) $stack_machine_src)"

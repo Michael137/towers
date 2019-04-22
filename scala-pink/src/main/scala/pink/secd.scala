7d6bdbf49a157bf5e6a0fd57d6b535eae02fb1a9
Nada Amin <namin@alum.mit.edu>
2019-04-22 17:30:29 +0100

    keep the ld convention by putting fun at end

--- scala-pink/src/main/scala/pink/secd.scala
+++ scala-pink/src/main/scala/pink/secd.scala
@@ -9,6 +9,9 @@ object SECD_Machine {
 (if (eq? y 1) (car lst) ((loc (- y 1)) (cdr lst)))))
 ((loc j) ((loc i) env))
 ))))
+(let append (lambda append xs (lambda _ ys
+  (if (eq?  '() xs) ys (cons (car xs) ((append (cdr xs)) ys)))))
+(let snoc (lambda snoc x (lambda _ xs ((append xs) (cons x '()))))
 (let machine (lambda machine s (lambda _ e (lambda _ c (lambda _ d
 (let _ (log 0 c)
 (if (eq? 'NIL (car c)) ((((machine (cons '() s)) e) (cdr c)) d)
@@ -27,7 +30,7 @@ object SECD_Machine {
 (if (eq? 'JOIN (car c)) ((((machine s) e) (car d)) (cdr d))
 (if (eq? 'LDF (car c))
   (let f (cadr c)
-  (let fun (lambda fun xs ((((machine '()) (cons (cons fun xs) e)) f) '()))
+  (let fun (lambda fun xs ((((machine '()) (cons ((snoc fun) xs) e)) f) '()))
   ((((machine (cons fun s)) e) (cddr c)) d)))
 (if (eq? 'AP (car c))
   (let fun (car s)
@@ -39,7 +42,7 @@ object SECD_Machine {
 (if (eq? 'STOP (car c)) s
 (if (eq? 'WRITEC (car c)) (car s)
 (cons 'ERROR c))))))))))))))))))))
-(lambda _ c ((((machine '()) '()) c) '()))))
+(lambda _ c ((((machine '()) '()) c) '()))))))
 """
 
   val evl = src
@@ -51,7 +54,7 @@ object SECD_Machine {
     check(ev(s"($evl '(LDC 1 LDC 2 ADD WRITEC))"))("Cst(3)")
     check(ev(s"($evl '(LDC 2 SEL (LDC 1 JOIN) (LDC 0 JOIN) WRITEC))"))("Cst(1)")
     check(ev(s"($evl '(NIL LDC 2 CONS LDC 1 CONS LDF (LDC 2 LDC 1 ADD RTN) AP WRITEC))"))("Cst(3)")
-    check(ev(s"($evl '(NIL LDC 2 CONS LDC 1 CONS LDF (LD (1 3) LD (1 2) ADD RTN) AP WRITEC))"))("Cst(3)") // note the loads are shited because of fun self-ref
-    check(ev(s"($evl '(NIL LDC 6 CONS LDF (LD (1 2) SEL (NIL LDC 1 LD (1 2) SUB CONS LD (1 1) AP LD (1 2) MPY JOIN) (LDC 1 JOIN) RTN) AP WRITEC))"))("Cst(720)")
+    check(ev(s"($evl '(NIL LDC 2 CONS LDC 1 CONS LDF (LD (1 2) LD (1 1) ADD RTN) AP WRITEC))"))("Cst(3)")
+    check(ev(s"($evl '(NIL LDC 6 CONS LDF (LD (1 1) SEL (NIL LDC 1 LD (1 1) SUB CONS LD (1 2) AP LD (1 1) MPY JOIN) (LDC 1 JOIN) RTN) AP WRITEC))"))("Cst(720)")
   }
 }
